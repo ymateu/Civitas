b80a5405944d6a6c0f9330db20185c71ec999f4b
lynkpoRK24 <leonardo.aragao@nave.org.br>
2026-09-14 11:46:32 -0300

    feat: society

---
+++ src/main/java/com/ymateu/civitas/society/Society.java
@@ -0,0 +1,13 @@
+package com.ymateu.civitas.society;
+
+public class Society {
+    private int governmentApproval;
+
+    public int getGovernmentApproval() {
+        return governmentApproval;
+    }
+
+    public void setGovernmentApproval(int governmentApproval) {
+        this.governmentApproval = governmentApproval;
+    }
+}
