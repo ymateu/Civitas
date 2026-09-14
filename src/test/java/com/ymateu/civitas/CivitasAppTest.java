855c2383bb363af32780706f3e775bd80e92ff73
ale <>
2026-09-14 10:06:22 -0300

    add: tests

---
+++ src/test/java/com/ymateu/civitas/CivitasAppTest.java
@@ -0,0 +1,38 @@
+package com.ymateu.civitas;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CivitasAppTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CivitasAppTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CivitasAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
