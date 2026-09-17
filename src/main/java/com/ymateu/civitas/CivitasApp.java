06e8eeb5faf904fa5f81ab1be3f563343a2704c0
ymateu <devmateu@gmail.com>
2026-09-17 16:28:34 -0300

    feat: UI start

--- src/main/java/com/ymateu/civitas/CivitasApp.java
+++ src/main/java/com/ymateu/civitas/CivitasApp.java
@@ -1,120 +1,43 @@
 package com.ymateu.civitas;
 
-import com.ymateu.civitas.engine.GameState;
-import com.ymateu.civitas.events.Event;
-import com.ymateu.civitas.events.EventChoice;
-import com.ymateu.civitas.events.EventManager;
-import com.ymateu.civitas.government.President;
-import com.ymateu.civitas.society.Society;
+import com.badlogic.gdx.Game;
+import com.ymateu.civitas.UI.LoadingScreen;
+import com.ymateu.civitas.UI.MainScreen;
+import com.ymateu.civitas.UI.MenuScreen;
+import com.ymateu.civitas.UI.PreferencesScreen;
 
-import java.util.Scanner;
+public class CivitasApp extends Game{
 
-public class CivitasApp {
+    private LoadingScreen loadingScreen;
+    private PreferencesScreen preferencesScreen;
+    private MenuScreen menuScreen;
+    private MainScreen mainScreen;
 
-    public static void main(String[] args) {
+    public final static int MENU = 0;
+    public final static int PREFERENCES = 1;
+    public final static int APPLICATION = 2;
 
-        GameState state = new GameState();
 
-        President president = new President();
-        president.setPopularity(50);
-
-        Society society = new Society();
-        society.setGovernmentApproval(20);
-
-        state.setPresident(president);
-        state.setSociety(society);
-
-        EventManager eventManager = new EventManager();
-        Scanner scanner = new Scanner(System.in);
-
-        boolean running = true;
-
-        while (running) {
-
-            clearScreen();
-
-            System.out.println("===== CIVITAS =====");
-            System.out.println("Popularidade: " + state.getPresident().getPopularity());
-            System.out.println("Aprovacao do governo: " + state.getSociety().getGovernmentApproval());
-            System.out.println();
-            System.out.println("1 - Iniciar evento");
-            System.out.println("2 - Aumentar aprovacao");
-            System.out.println("3 - Diminuir aprovacao");
-            System.out.println("4 - Sair");
-            System.out.print("Escolha: ");
-
-            String input = scanner.nextLine();
-
-            switch (input) {
-
-                case "1":
-                    clearScreen();
-
-                    Event event = eventManager.generateEvent(state);
-
-                    if (event == null) {
-                        System.out.println("Nenhum evento pode ocorrer neste momento.");
-                        break;
-                    }
-
-                    System.out.println("===== EVENTO =====");
-                    System.out.println(event.getTitle());
-                    System.out.println(event.getDescription());
-                    System.out.println();
-
-                    EventChoice[] choices = event.getChoices();
-
-                    for (int i = 0; i < choices.length; i++) {
-                        System.out.println((i + 1) + " - " + choices[i].getDescription());
-                    }
-
-                    System.out.print("Escolha: ");
-
-                    try {
-                        int choice = Integer.parseInt(scanner.nextLine());
-
-                        if (choice >= 1 && choice <= choices.length) {
-                            choices[choice - 1].execute(state);
-                        } else {
-                            System.out.println("Escolha invalida.");
-                        }
-
-                    } catch (NumberFormatException e) {
-                        System.out.println("Digite um numero valido.");
-                    }
-
-                    break;
-
-                case "2":
-                    state.getSociety().setGovernmentApproval(
-                            state.getSociety().getGovernmentApproval() + 10
-                    );
-                    break;
-
-                case "3":
-                    state.getSociety().setGovernmentApproval(
-                            state.getSociety().getGovernmentApproval() - 10
-                    );
-                    break;
-
-                case "4":
-                    running = false;
-                    clearScreen();
-                    System.out.println("Saindo do Civitas...");
-                    break;
-
-                default:
-                    System.out.println("Opcao invalida.");
-                    break;
-            }
-        }
-
-        scanner.close();
+    @Override
+    public void create () {
+        loadingScreen = new LoadingScreen(this);
+        setScreen(loadingScreen);
     }
 
-    private static void clearScreen() {
-        for (int i = 0; i < 50; i++) {
-            System.out.println();
+    public void changeScreen(int screen){
+        switch(screen){
+            case MENU:
+                if(menuScreen == null) menuScreen = new MenuScreen(this);
+                this.setScreen(menuScreen);
+                break;
+            case PREFERENCES:
+                if(preferencesScreen == null) preferencesScreen = new PreferencesScreen(this);
+                this.setScreen(preferencesScreen);
+                break;
+            case APPLICATION:
+                if(mainScreen == null) mainScreen = new MainScreen(this);
+                this.setScreen(mainScreen);
+                break;
         }
     }
 }
