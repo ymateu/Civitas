2464f56c0d24665dcfb742846684a3b7f4ace582
ymateu <devmateu@gmail.com>
2026-09-17 16:27:22 -0300

    feat: desktop launcher

---
+++ src/main/java/com/ymateu/civitas/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.ymateu.civitas;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+
+    public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration config =
+                new Lwjgl3ApplicationConfiguration();
+
+        config.setTitle("Civitas");
+        config.setWindowedMode(1280, 720);
+        config.setForegroundFPS(60);
+
+        new Lwjgl3Application(new CivitasApp(), config);
+    }
+}
