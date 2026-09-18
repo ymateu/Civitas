ee16f7834a67ee030165e739a96b9eaf5680c73e
ymateu <devmateu@gmail.com>
2026-09-18 00:22:18 -0300

    feat: preference screen

--- src/main/java/com/ymateu/civitas/UI/PreferencesScreen.java
+++ src/main/java/com/ymateu/civitas/UI/PreferencesScreen.java
@@ -1,26 +1,104 @@
 package com.ymateu.civitas.UI;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.ymateu.civitas.CivitasApp;
+import com.badlogic.gdx.scenes.scene2d.Event;
 
 public class PreferencesScreen implements Screen {
 
     private CivitasApp app;
+    private Stage stage;
+
+    private Label titleLabel;
+    private Label volumeMusicLabel;
+    private Label volumeSoundLabel;
+    private Label musicOnOffLabel;
 
     public PreferencesScreen(CivitasApp civitasApp) {
         app = civitasApp;
+
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void show() {
+        Skin skin = new Skin(Gdx.files.internal("skin/comic-ui.json"));
+
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        final Slider volumeMusicSlider = new Slider( 0f, 1f, 0.1f,false, skin );
+        final CheckBox musicCheckbox = new CheckBox(null, skin);
+        final TextButton backButton = new TextButton("Voltar", skin);
+
+        volumeMusicSlider.setValue( app.getPreferences().getMusicVolume() );
+        volumeMusicSlider.addListener( new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                app.getPreferences().setMusicVolume( volumeMusicSlider.getValue() );
+                return false;
+            }
+        });
+
+        musicCheckbox.setChecked( app.getPreferences().isMusicEnabled() );
+        musicCheckbox.addListener( new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                boolean enabled = musicCheckbox.isChecked();
+                app.getPreferences().setMusicEnabled( enabled );
+                return false;
+            }
+        });
+
+        backButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                app.changeScreen(CivitasApp.MENU);
+            }
+        });
+
+
+        titleLabel = new Label( "Preferences", skin );
+        volumeMusicLabel = new Label( null, skin );
+        volumeSoundLabel = new Label( null, skin );
+        musicOnOffLabel = new Label( null, skin );
+
+        table.add(titleLabel);
+        table.row();
+        table.add(volumeMusicLabel);
+        table.add(volumeMusicSlider);
+        table.row();
+        table.add(musicOnOffLabel);
+        table.add(musicCheckbox);
+        table.row();
+        table.add(volumeSoundLabel);
+        table.row();
+        table.add(backButton);
+
     }
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
@@ -37,6 +115,7 @@ public class PreferencesScreen implements Screen {
 
     @Override
     public void dispose() {
+        stage.dispose();
     }
 
 }
